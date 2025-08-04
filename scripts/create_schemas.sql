669a7ac2b79f2cc7fd6829477732f8577a444669
dmls <76413717+diogoo-lemos@users.noreply.github.com>
2025-08-04 16:58:52 +0100

    Create create_schemas.sql

---
+++ scripts/create_schemas.sql
@@ -0,0 +1,12 @@
+/*
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Create Schemas
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+This script creates three schemas within the database.
+*/
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
