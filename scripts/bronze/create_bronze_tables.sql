cf4d81eba94fd94755170726a15771a3272b4f5d
dmls <76413717+diogoo-lemos@users.noreply.github.com>
2025-08-04 18:03:49 +0100

    Update create_bronze_tables.sql

--- scripts/bronze/create_bronze_tables.sql
+++ scripts/bronze/create_bronze_tables.sql
@@ -9,9 +9,7 @@ If the table does not exist it will CREATE it. If not,
 it will DROP it.
 */
 
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_prd_info;
-GO
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 
 CREATE TABLE bronze.crm_prd_info(
 	prd_id INT,
@@ -22,11 +20,8 @@ CREATE TABLE bronze.crm_prd_info(
 	prd_start_dt date,
 	prd_end_dt date
 );
-GO
 
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_cust_info;
-GO
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 
 CREATE TABLE bronze.crm_cust_info(
 	cst_id INT,
@@ -37,11 +32,8 @@ CREATE TABLE bronze.crm_cust_info(
 	cst_gndr VARCHAR(50),
 	cst_create_date date
 );
-GO
 
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_sales_details;
-GO
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 
 CREATE TABLE bronze.crm_sales_details(
 	sls_ord_num VARCHAR(50),
@@ -54,32 +46,23 @@ CREATE TABLE bronze.crm_sales_details(
 	sls_quantity INT,
 	sls_price INT
 );
-GO
 
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_cust_az12;
-GO
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
 
 CREATE TABLE bronze.erp_cust_az12(
 	cid VARCHAR(50),
 	bdate date,
 	gen VARCHAR(50)
 );
-GO
 
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_loc_a101;
-GO
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
 
 CREATE TABLE bronze.erp_loc_a101(
 	cid VARCHAR(50),
 	cntry VARCHAR(50)
 );
-GO
 
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_px_cat_g1v2;
-GO
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 
 CREATE TABLE bronze.erp_px_cat_g1v2(
 	id VARCHAR(50),
@@ -87,4 +70,3 @@ CREATE TABLE bronze.erp_px_cat_g1v2(
 	subcat VARCHAR(50),
 	maintenance VARCHAR(50)
 );
-GO
